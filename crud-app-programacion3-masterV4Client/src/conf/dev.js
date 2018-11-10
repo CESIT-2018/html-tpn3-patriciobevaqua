1e0d150ffeccfab674ab0eb7bb6de2410d53ed6c
Patricio Ariel Bevaqua <patriciobq@gmail.com>
2018-11-10 16:14:24 -0300

    arreglando commit

--- crud-app-programacion3-masterV4Client/src/conf/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    PORT: 5000,
-    mongoURI: "mongodb://localhost:27017/fpp-dev"
-};
--- crud-app-programacion3/src/conf/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    PORT: 5000,    //puerto en que levanta el servidor de express
-    mongoURI: "mongodb://localhost:27017/fpp-dev"    //fpp-dev es un nombre cualquiera
-};
